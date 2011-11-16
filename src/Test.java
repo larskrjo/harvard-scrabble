07eced269de724cd2da797a4c79f386d984399da
Lars Kristian Johansen <larskrjo@gmail.com>
2011-11-16 18:24:33 -0500

    first real commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: larsen
+ * Date: 11/16/11
+ * Time: 6:20 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+	public static void main(String[] args){
+		System.out.println("test");
+	}
+
+}
