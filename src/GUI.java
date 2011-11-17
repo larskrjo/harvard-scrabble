1aa9bb667ed962293548ce96f0213be913c4f3bc
Havard Normann <havard.h.normann@gmail.com>
2011-11-16 20:12:09 -0500

    Jeg har ikke gjort noe fornuftig.

---
+++ src/GUI.java
@@ -0,0 +1,9 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: havard_normann
+ * Date: 16.11.11
+ * Time: 20.11
+ * To change this template use File | Settings | File Templates.
+ */
+public class GUI {
+}
